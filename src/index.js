430e2c838c2cad256ab8a05391c3266452925650
adiKhan12 <ghories555@gmail.com>
2019-01-17 03:19:50 -0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 //https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#adding-a-stylesheet
-//import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css';
 //import 'bootstrap/dist/css/bootstrap-theme.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
